bf2a11a34fed0d8dffcb34b70c07197ed414ac10
utkarsh-2033 <utkarshg@2033>
2025-04-06 01:21:20 +0530

    minor app.py update

--- backend/RecommendationModel/app.py
+++ backend/RecommendationModel/app.py
@@ -162,5 +162,5 @@ def book_recommendation():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 3000))
     app.run(host="0.0.0.0", port=port)
